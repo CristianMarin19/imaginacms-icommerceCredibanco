105a6a0b49edd4054b54e07fbcb8f6dea32abe45
msolanogithub <msolano@imaginacolombia.com>
2024-09-03 14:48:29 -0500

    added function calculate to cacheDecorator

--- Repositories/Cache/CacheIcommerceCredibancoDecorator.php
+++ Repositories/Cache/CacheIcommerceCredibancoDecorator.php
@@ -7,10 +7,17 @@ use Modules\Core\Repositories\Cache\BaseCacheDecorator;
 
 class CacheIcommerceCredibancoDecorator extends BaseCacheDecorator implements IcommerceCredibancoRepository
 {
-    public function __construct(IcommerceCredibancoRepository $icommercecredibanco)
-    {
-        parent::__construct();
-        $this->entityName = 'icommercecredibanco.icommercecredibancos';
-        $this->repository = $icommercecredibanco;
-    }
+  public function __construct(IcommerceCredibancoRepository $icommercecredibanco)
+  {
+    parent::__construct();
+    $this->entityName = 'icommercecredibanco.icommercecredibancos';
+    $this->repository = $icommercecredibanco;
+  }
+
+  public function calculate($parameters, $conf)
+  {
+    return $this->remember(function () use ($parameters, $conf) {
+      return $this->repository->calculate($parameters, $conf);
+    });
+  }
 }
